d12e22d4a84a8c4796ef5ab27801db49fc38399e
annanya17699 <annanya17699@gmail.com>
2022-02-06 12:59:41 +0530

    express

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.use((req, res, next)=>{
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+ next();
+})
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@home-and-dome.0mqzc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+.then(()=>{
+ console.log("connected to db");
+ app.listen(process.env.PORT || 5000);
+})
+.catch(err=> console.log(err));
